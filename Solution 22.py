f8557d4ca118259cc1abf6d7ab12ab57e39906ea
Jagroop Singh <131522392+jagroop-singh0074@users.noreply.github.com>
2024-09-30 05:56:16 +1300

    Create Solution 22.py

---
+++ Solution 22.py
@@ -0,0 +1,14 @@
+public class Solution:
+    def maxValue(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        dp = [[0] * (k + 1) for _ in range(n + 1)]
+        
+        for i in range(1, n + 1):
+            or_val = 0
+            for j in range(min(i, k), 0, -1):
+                or_val |= nums[i - 1]
+                dp[i][j] = max(dp[i][j], dp[i - j][i - j] ^ or_val)
+                for l in range(1, j):
+                    dp[i][j] = max(dp[i][j], dp[i - l][i - l] ^ (or_val | nums[i - l - 1]))
+        
+        return dp[n][k]
